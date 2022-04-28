b0a6b09016afd105a1b15056649fa981660969f7
Jamiell Joseph <josephjamiell@gmail.com>
2022-04-27 22:28:59 -0400

    Added API routes for application

--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,6 @@
-import express from 'express';
+import 'dotenv/config';
+import fetch from 'node-fetch';
+import express, { application, response, response, response } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import path from 'path';
@@ -18,6 +20,66 @@ app.use(cors());
 
 app.get('/', (req, res) => {
     res.sendFile('dist/index.html');
+})
+
+app.get('/weather', (req, res) => {
+    const lat = req.query.lat;
+    const lon = req.query.lon;
+
+    if(lat === null || lon === null) {
+        res.send("Invalid request. Arguments missing.");
+    }
+
+    const response = fetch(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.WEATHERBIT_API_KEY}`)
+        .then((response) => response.json())
+        .then((data) => {res.send(data)})
+        .catch((err) => {
+            console.error(err);
+            console.log(`Failed to aquire weather data for lat: ${lat} long: ${lon}`);
+        })
+})
+
+app.get('/geo', (req, res) => {
+    const placename = req.query.placename;
+
+    if(placename === null) {
+        res.send("Invalid request. Arguments missing.");
+    }
+
+    const response = fetch(`http://api.geonames.org/postalCodeSearchJSON?placename=${placename}&country=us&username=${process.env.GEONAMES_USERNAME}`)
+        .then((response) => response.json())
+        .then((data) => {
+            let locations = data.postalCodes.map(x => {return {
+                city: x.placeName, 
+                state: x.adminName1,
+                lat: x.lat,
+                lon: x.lon
+            }});
+            res.send(locations)
+        })
+        .catch((err) => {
+            console.error(err);
+            console.log(`Failed to aquire data for places starting with ${placename}`);
+        })
+})
+
+app.get('/images', (req, res) => {
+
+    const q = req.query.q;
+
+    if(placename === null) {
+        res.send("Invalid request. Arguments missing.");
+    }
+
+    const response = fetch(`https://pixabay.com/api/?q=leopard&key=${process.env.PIXABAY_API_KEY}`)
+        .then((response) => response.json())
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            console.err(err);
+            console.log("Failed to retrieve photos");
+        })
 })
 
 app.listen(port, () => {
